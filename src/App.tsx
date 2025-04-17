3f9050b1f4bdd35b78ffd76fb68086fcd6691181
Arnav Surve <ec2-user@grace-lang.org>
2025-04-17 07:45:57 +0000

    ye

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <>
       <div>
       	<h1>The Grace Programming Language</h1>
+	<p>We can build forward by looking back with curiosity and care.</p>
       </div>
     </>
   )
